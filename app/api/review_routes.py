067db35ca666910842223c143f2b47dd264d97df
Jacob Stodtmeister <stodtmeister@gmail.com>
2024-02-05 16:35:53 -0700

    added and tested backend review routes to get, post, edit, and delete

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -65,3 +65,66 @@ def submit_review(bookId):
         return jsonify(new_review.to_dict(include_books=False)), 201
 
     return {"errors": validation_errors_to_error_messages(form.errors)}, 400
+
+
+"""
+Handles the editing of an existing book review.
+
+Parameters:
+bookId (int): The ID of the book whose review is being edited.
+reviewId (int): The ID of the review to be edited.
+
+Returns:
+A JSON response containing the data of the updated review if the form validation is successful,
+or an error message if the user is not the author of the review or the form validation fails.
+"""
+@review_routes.route('/<int:bookId>/reviews/<int:reviewId>', methods=["PUT"])
+@login_required
+def edit_review(bookId, reviewId):
+    review = Review.query.get(reviewId)
+
+    if review is None:
+        return {"errors": ["Review not found"]}
+
+    if current_user.id != review.user_id:
+        return {"forbidden": "You can only edit your own reviews"}, 403
+
+    form = ReviewForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+
+    if form.validate_on_submit():
+        review.review = form.data['review']
+        review.stars = form.data['stars']
+
+        db.session.commit()
+        return jsonify(review.to_dict(include_books=False)), 201
+
+    return {"errors": validation_errors_to_error_messages(form.errors)}, 400
+
+
+"""
+Handles the deletion of an existing book review.
+
+Parameters:
+bookId (int): The ID of the book whose review is being deleted.
+reviewId (int): The ID of the review to be deleted.
+
+Returns:
+A JSON response with a success message if the review is successfully deleted, or an error message
+if the user is not the author of the review or the review does not exist.
+"""
+@review_routes.route('/<int:bookId>/reviews/<int:reviewId>', methods=['DELETE'])
+@login_required
+def delete_review(bookId, reviewId):
+    review = Review.query.get(reviewId)
+
+    if review is None:
+        return {"errors": ["Review not found"]}
+
+    if current_user.id != review.user_id:
+        return {"forbidden": "You can only edit your own reviews"}, 403
+
+    db.session.delete(review)
+    db.session.commit()
+
+    return {"message": "This review has been deleted"}
