bae8d1b7afc0953ade2eae9717e2a73122f09593
Jacob Stodtmeister <stodtmeister@gmail.com>
2024-02-09 11:27:24 -0700

    added thunkAddBoard to the redux board store

--- react-vite/src/pages/Boards/Boards.jsx
+++ react-vite/src/pages/Boards/Boards.jsx
@@ -1,27 +1,33 @@
 import { useDispatch, useSelector } from 'react-redux';
 import './Boards.css'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { thunkGetBoards } from '../../redux/boards';
+import OpenModalMenuItem from '../../components/Navigation/OpenModalMenuItem';
+import LoginFormModal from '../../components/LoginFormModal';
 
 export default function Boards() {
   const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
   const boards = useSelector(state => state.boards.boards);
+
+  function newBoard() {
+
+  }
+  const closeMenu = () => setShowMenu(false);
 
   useEffect(() => {
     dispatch(thunkGetBoards());
   }, [dispatch]);
 
-  // return (
-  //   <div className=".boards-container">
-  //     <h1>Boards</h1>
-  //       {boards?.map((board, index) => (
-  //         <div key={board.id} className="boards">{board.name}</div>
-  //       ))}
-  //   </div>
-  // )
   return (
     <div className="boards-container">
       <h1>Boards</h1>
+      <OpenModalMenuItem
+        itemText="Add board"
+        onItemClick={closeMenu}
+        modalComponent={<LoginFormModal />}
+      />
+      <button onClick={newBoard}>Add New Board</button>
       <div className="boards-grid">
         {boards?.map((board, index) => (
           <div key={board.id} className="board">{board.name}</div>
--- react-vite/src/redux/boards.js
+++ react-vite/src/redux/boards.js
@@ -1,9 +1,25 @@
 const GET_BOARDS = 'GET_BOARDS';
+const ADD_BOARD = 'ADD_BOARD'
+const BOARD_BY_ID = 'BOARD_BY_ID'
 
 export const getBoards = (boards) => {
   return {
     type: GET_BOARDS,
     boards
+  }
+}
+
+export const addBoard = (board) => {
+  return {
+    type: ADD_BOARD,
+    board
+  }
+}
+
+export const getBoardById = (books) => {
+  return {
+    type: BOARD_BY_ID,
+    books
   }
 }
 
@@ -15,13 +31,32 @@ export const thunkGetBoards = () => async (dispatch) => {
   }
 }
 
+export const thunkAddBoard = (data) => async (dispatch) => {
+  const response = await fetch('/api/boards/', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data)
+  })
+
+  if (response.ok) {
+    const data = await response.json()
+    dispatch(addBoard(data))
+  } else {
+    console.log('ERROR (thunkAddBoard)')
+  }
+}
+
 const initialState = { boards: [] };
 
 export default function boardsReducer(state = initialState, action) {
   switch (action.type) {
     case GET_BOARDS:
-      console.log('BOARD REDUCER', action.boards);
+      // console.log('BOARD REDUCER', action.boards);
       return { ...state, boards: action.boards.boards };
+    case ADD_BOARD:
+      return { ...state, boards: [...state.boards, action.board] }
     default:
       return state;
   }
