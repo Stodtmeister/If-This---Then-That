2c76fa340020d60cb66c596c4d43e8e9bd897004
Jacob Stodtmeister <stodtmeister@gmail.com>
2024-02-12 18:41:18 -0700

    added the ability to edit and delete a board

--- react-vite/src/pages/Boards/Boards.jsx
+++ react-vite/src/pages/Boards/Boards.jsx
@@ -1,78 +1,7 @@
-// import { useDispatch, useSelector } from 'react-redux'
-// import { useEffect, useState } from 'react'
-// import { useNavigate } from 'react-router-dom'
-// import { thunkGetBoards, thunkGetBoardById, thunkEditBoard } from '../../redux/boards'
-// import OpenModalButton from '../../components/OpenModalButton/OpenModalButton'
-// import NewBoardModal from '../../components/NewBoardModal/NewBoardModal'
-// import './Boards.css'
-
-// export default function Boards() {
-//   const dispatch = useDispatch()
-//   const [showMenu, setShowMenu] = useState(false)
-//   const [showOptions, setShowOptions] = useState(false)
-//   const boards = useSelector((state) => state.boards.boards)
-//   const navigate = useNavigate()
-
-//   const closeMenu = () => setShowMenu(false)
-
-//   function handleEdit(boardId) {
-//     dispatch(thunkEditBoard(boardId, { name: 'Gandalfe' }))
-//   }
-
-//   function handleDelete() {
-//     console.log('delete')
-//   }
-
-//   useEffect(() => {
-//     dispatch(thunkGetBoards())
-//   }, [dispatch])
-
-//   return (
-//     <div className="boards-container">
-//       <h1>Boards</h1>
-//       <OpenModalButton
-//         buttonText="Add board"
-//         onItemClick={closeMenu}
-//         modalComponent={<NewBoardModal />}
-//       />
-//       <div className="boards-grid">
-//         {boards?.map((board, index) => (
-//           <div
-//             key={board.id}
-//             className="board"
-//             onClick={() => navigate(`/boards/${board.id}`)}
-//             >
-//             {board.name}
-//             <i
-//               className="fa-solid fa-ellipsis-vertical"
-//               onClick={(e) => {
-//                 e.stopPropagation()
-//                 setShowOptions(!showOptions)
-//               }}
-//             ></i>
-//             {showOptions && (
-//               <div className="board-options">
-//                 <button className='edit-board-btn' onClick={(e) => {
-//                   e.stopPropagation()
-//                   handleEdit(board.id)
-//                 }}>Edit</button>
-//                 <button className='delete-board-btn' onClick={(e) => {
-//                   e.stopPropagation()
-//                   handleDelete(board.id)
-//                 }}>Delete</button>
-//               </div>
-//             )}
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   )
-// }
-
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { thunkGetBoards, thunkGetBoardById, thunkEditBoard, thunkDeleteBoard } from '../../redux/boards'
+import { thunkGetBoards, thunkEditBoard, thunkDeleteBoard } from '../../redux/boards'
 import OpenModalButton from '../../components/OpenModalButton/OpenModalButton'
 import NewBoardModal from '../../components/NewBoardModal/NewBoardModal'
 import './Boards.css'
@@ -117,7 +46,7 @@ export default function Boards() {
         modalComponent={<NewBoardModal />}
       />
       <div className="boards-grid">
-        {boards?.map((board, index) => (
+        {boards?.map((board) => (
           <div
             key={board.id}
             className="board"
