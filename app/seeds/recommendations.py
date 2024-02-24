adf1bc45054f79395ce6d7d3da3be29ee0d393c2
Jacob Stodtmeister <stodtmeister@gmail.com>
2024-02-24 15:19:13 -0700

    changed it back

--- app/seeds/recommendations.py
+++ app/seeds/recommendations.py
@@ -1,19 +1,19 @@
-# from app.models import db, Recommendation, environment, SCHEMA
-# from sqlalchemy.sql import text
+from app.models import db, Recommendation, environment, SCHEMA
+from sqlalchemy.sql import text
 
 
-# def seed_recommendations():
-#     recommendation1 = Recommendation()
-#     recommendation2 = Recommendation()
-#     recommendation3 = Recommendation()
+def seed_recommendations():
+    recommendation1 = Recommendation()
+    recommendation2 = Recommendation()
+    recommendation3 = Recommendation()
 
-#     db.session.add_all([recommendation1, recommendation2, recommendation3])
-#     db.session.commit()
+    db.session.add_all([recommendation1, recommendation2, recommendation3])
+    db.session.commit()
 
-# def undo_recommendations():
-#     if environment == "production":
-#         db.session.execute(f"TRUNCATE table {SCHEMA}.recommendations RESTART IDENTITY CASCADE;")
-#     else:
-#         db.session.execute(text("DELETE FROM recommendations"))
+def undo_recommendations():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.recommendations RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM recommendations"))
 
-#     db.session.commit()
+    db.session.commit()
