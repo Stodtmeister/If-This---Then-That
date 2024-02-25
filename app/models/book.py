b00f0034c25262029c328da44a1abc90eb95c8c9
Jacob Stodtmeister <stodtmeister@gmail.com>
2024-02-24 18:32:17 -0700

    formatting

--- app/models/book.py
+++ app/models/book.py
@@ -14,11 +14,11 @@ class BookRecommendation(db.Model):
 
     bookRecommendation_id = db.Column(db.Integer, primary_key=True)
     book_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("books.id")))
-    recommendation_id = db.Column(db.Integer,db.ForeignKey(add_prefix_for_prod("recommendations.id")))
+    #!!!! recommendation_id = db.Column(db.Integer,db.ForeignKey(add_prefix_for_prod("recommendations.id")))
     votes = db.Column(db.Integer, nullable=True, default=1)
 
     book = db.relationship("Book", back_populates="book_recommendations")
-    recommendation = db.relationship("Recommendation", back_populates="book_recommendations")
+    #!!!! recommendation = db.relationship("Recommendation", back_populates="book_recommendations")
 
     def to_dict(self, include_books=True, include_recommendations=True):
         data = {
@@ -36,51 +36,9 @@ class BookRecommendation(db.Model):
                 include_recommendations=False,
                 include_series=False,
             )
-        if include_recommendations:
-            data["recommendation"] = self.recommendation.to_dict()
+        #!! if include_recommendations:
+        #!!     data["recommendation"] = self.recommendation.to_dict()
         return data
-
-# !Original
-# class BookRecommendation(db.Model):
-#     __tablename__ = "book_recommendation"
-
-#     if environment == "production":
-#         __table_args__ = {"schema": SCHEMA}
-
-#     book_id = db.Column(
-#         db.Integer, db.ForeignKey(add_prefix_for_prod("books.id")), primary_key=True
-#     )
-#     recommendation_id = db.Column(
-#         db.Integer,
-#         db.ForeignKey(add_prefix_for_prod("recommendations.id")),
-#         primary_key=True,
-#     )
-#     votes = db.Column(db.Integer, nullable=True, default=1)
-
-#     book = db.relationship("Book", back_populates="book_recommendations")
-#     recommendation = db.relationship(
-#         "Recommendation", back_populates="book_recommendations"
-#     )
-
-#     def to_dict(self, include_books=True, include_recommendations=True):
-#         data = {
-#             "bookId": self.book_id,
-#             "recommendationId": self.recommendation_id,
-#             "votes": self.votes,
-#         }
-
-#         if include_books:
-#             data["book"] = self.book.to_dict(
-#                 include_author=False,
-#                 include_boards=False,
-#                 include_reviews=False,
-#                 include_recommendations=False,
-#                 include_series=False,
-#             )
-#         if include_recommendations:
-#             data["recommendation"] = self.recommendation.to_dict()
-#         return data
-
 
 class Book(db.Model):
     __tablename__ = "books"
