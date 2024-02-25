09bce8d6433308c90d7826d644395074e3e49c3d
Jacob Stodtmeister <stodtmeister@gmail.com>
2024-02-24 20:12:18 -0700

    fixed the signup page

--- react-vite/src/components/SignupFormPage/SignupFormPage.jsx
+++ react-vite/src/components/SignupFormPage/SignupFormPage.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Navigate, useNavigate } from 'react-router-dom'
 import { thunkSignup } from '../../redux/session'
+import './SignupForm.css'
 
 function SignupFormPage() {
   const dispatch = useDispatch()
@@ -12,6 +13,8 @@ function SignupFormPage() {
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
   const [errors, setErrors] = useState({})
+  const firstNameRef = useRef(null)
+  const lastNameRef = useRef(null)
 
   if (sessionUser) return <Navigate to="/" replace={true} />
 
@@ -27,6 +30,8 @@ function SignupFormPage() {
 
     const serverResponse = await dispatch(
       thunkSignup({
+        first_name: firstNameRef.current.value,
+        last_name: lastNameRef.current.value,
         email,
         username,
         password,
@@ -36,31 +41,37 @@ function SignupFormPage() {
     if (serverResponse) {
       setErrors(serverResponse)
     } else {
-      navigate('/')
+      navigate('/boards')
     }
   }
 
   return (
     <div className="login-background">
       <h1 className="site-title">If This Then That</h1>
-      {/* <section className="logo-container">
-        <div className="container">
-          <div className="circle">
-            {[...Array(21)].map((_, i) => (
-              <span key={i} style={{ '--i': i }}></span>
-            ))}
-          </div>
-          <div className="circle">
-            {[...Array(21)].map((_, i) => (
-              <span key={i} style={{ '--i': i }}></span>
-            ))}
-          </div>
-        </div>
-      </section> */}
       {errors.server && <p>{errors.server}</p>}
       <form onSubmit={handleSubmit} className="login-form">
         <fieldset>
           <legend>Sign Up</legend>
+          <div className='first-and-last'>
+            <label className='signup-label'>
+              First Name:
+              <input
+                type="text"
+                ref={firstNameRef}
+                className="form-input first-name"
+                required
+              />
+            </label>
+            <label className='signup-label'>
+              Last Name:
+              <input
+                type="text"
+                ref={lastNameRef}
+                className="form-input last-name"
+                required
+              />
+            </label>
+          </div>
           <label className="signup-label">
             Email:
             <input
