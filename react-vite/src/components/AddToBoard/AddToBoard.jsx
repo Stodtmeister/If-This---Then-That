b0824735332a3bee134b4e306975e5f9becfc612
Jacob Stodtmeister <stodtmeister@gmail.com>
2024-02-25 00:19:45 -0700

    added some styling to the recommendations page

--- react-vite/src/components/AddToBoard/AddToBoard.jsx
+++ react-vite/src/components/AddToBoard/AddToBoard.jsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { thunkAddBook, thunkGetBoards } from "../../redux/boards";
 import { useEffect } from "react";
 import { toast } from "react-toastify";
+import './AddToBoard.css'
 
 export default function AddToBoard({ bookId, setClickedBookId }) {
   // console.log(bookId, 'BOOKID');
@@ -19,15 +20,20 @@ export default function AddToBoard({ bookId, setClickedBookId }) {
   }
 
   return (
-    <>
-      <h2>My Boards</h2>
-      <div>
+    <div className="board-choices">
+      <select className="the-select" onChange={(e) => {
+        const selectedBoardId = Number(e.target.value);
+        const selectedBoard = boards.find(board => board.id === selectedBoardId);
+        if (selectedBoard) {
+          handleMoveBook(selectedBoard.id, selectedBoard.name);
+        }
+      }}>
+        <option value="">Select a board</option>
         {boards?.map((board) => (
-          <div key={board.id}>
-            <button onClick={() => handleMoveBook(board.id, board.name)}>Add to {board.name}</button>
-          </div>
+          <option key={board.id} value={board.id}>Add to {board.name}</option>
         ))}
-      </div>
-    </>
+      </select>
+      <button className="cancel-choices" onClick={() => setClickedBookId(null)}>X</button>
+    </div>
   )
 }
