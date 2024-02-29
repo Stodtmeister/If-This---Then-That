5969248f024b5911587cb2e6f2dd809e4c8798b8
Jacob Stodtmeister <stodtmeister@gmail.com>
2024-02-28 19:01:07 -0700

    removed console logs

--- react-vite/src/components/AddRecommendation/AddRecommendation.jsx
+++ react-vite/src/components/AddRecommendation/AddRecommendation.jsx
@@ -95,11 +95,9 @@ export default function AddRecommendation() {
       }
 
       if (book.cover) {
-        console.log('Has cover')
         setBookCovers((prev) => ({ ...prev, [book.id]: book.cover }))
         return Promise.resolve()
       } else {
-        console.log('Fetch cover')
         const response = await fetch(
           `https://www.googleapis.com/books/v1/volumes?q=intitle:${encodeURIComponent(
             book.title
